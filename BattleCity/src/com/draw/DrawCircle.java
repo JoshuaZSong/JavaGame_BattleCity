8cd9acc72ae58f73b26f5781b102f050743a8919
JoshuaZSong <joshuasong94@outlook.com>
2024-12-13 16:09:52 -0700

    Create DrawCircle.java

---
+++ BattleCity/src/com/draw/DrawCircle.java
@@ -0,0 +1,19 @@
+package com.draw;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawCircle {
+    public static void main(String[] args){
+
+    }
+}
+
+//1.MyPanel class
+class MyPanel extends JPanel{
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+}
