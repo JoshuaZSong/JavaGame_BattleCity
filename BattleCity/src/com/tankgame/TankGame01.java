de05fc963a550c1c37ba1666c894c696472e3839
JoshuaZSong <joshuasong94@outlook.com>
2024-12-25 10:54:28 -0700

    Create TankGame01.java

---
+++ BattleCity/src/com/tankgame/TankGame01.java
@@ -0,0 +1,15 @@
+package com.tankgame;
+
+import javax.swing.*;
+
+public class TankGame01 extends JFrame {
+
+    MyPanel mp = null;
+    public static void main(String[] args){
+
+    }
+
+    public TankGame01(){
+        mp = new MyPanel();
+    }
+}
