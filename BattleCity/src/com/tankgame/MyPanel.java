73d6afe8a71732d71002eaba3796d3ff1884cfc3
JoshuaZSong <joshuasong94@outlook.com>
2024-12-27 09:45:47 -0700

    draw tanks

--- BattleCity/src/com/tankgame/MyPanel.java
+++ BattleCity/src/com/tankgame/MyPanel.java
@@ -16,6 +16,27 @@ public class MyPanel extends JPanel {
     @Override
     public void paint(Graphics g){
         super.paint(g);
-        g.fillRect(0,0,100,750);
+        g.fillRect(0,0,1000,750);
+    }
+
+    /**
+     *
+     * @param x tank's top-left conor x
+     * @param y tank's top-left conor y
+     * @param g draw
+     * @param direct direction
+     * @param type hero/enemy
+     */
+    public void drawTank(int x, int y,Graphics g, int direct, int type){
+
+        switch(type){
+            case 0: // hero
+                g.setColor(Color.cyan);
+                break;
+            case 1: //enemy tank
+                g.setColor(Color.yellow);
+                break;
+
+        }
     }
 }
