279925380d53a71943cc78a5b1d0bd6cb5a29483
JoshuaZSong <joshuasong94@outlook.com>
2024-12-27 09:59:22 -0700

    Update MyPanel.java

--- BattleCity/src/com/tankgame/MyPanel.java
+++ BattleCity/src/com/tankgame/MyPanel.java
@@ -19,7 +19,11 @@ public class MyPanel extends JPanel {
         super.paint(g);
         g.fillRect(0, 0, 1000, 750);
 
+        //hero tank
         drawTank(hero.getX(), hero.getY(), g, 0, 0);
+
+        //enemy tank
+        drawTank(hero.getX() + 60, hero.getY(), g, 0, 1);
     }
 
     /**
@@ -46,7 +50,7 @@ public class MyPanel extends JPanel {
                 g.fill3DRect(x + 30, y, 10, 60, false);//tank's left track
                 g.fill3DRect(x + 10, y + 10, 20, 40, false);//tank's body
                 g.fillOval(x + 9, y + 19, 20, 20);//tank's cabin
-                g.drawLine(x + 19, y + 30, x + 19, y);
+                g.drawLine(x + 19, y + 30, x + 19, y);//tank's cannon
                 break;
             default:
         }
