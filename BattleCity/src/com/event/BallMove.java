634b482ec26dad02022cf2f38dba85bcc0678a8f
JoshuaZSong <joshuasong94@outlook.com>
2024-12-30 21:28:54 -0700

    Update BallMove.java

--- BattleCity/src/com/event/BallMove.java
+++ BattleCity/src/com/event/BallMove.java
@@ -3,24 +3,28 @@ package com.event;
 import javax.swing.*;
 import java.awt.*;
 
-public class BallMove extends JFrame{
+public class BallMove extends JFrame {
+    private final MyPanel myPanel;
     MyPanel mp = null;
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        BallMove ballMove = new BallMove();
     }
 
-    public BallMove(){
-        mp = new MyPanel();
+    public BallMove() {
+        myPanel;
         this.add(mp);
-        this.setSize(400,300);
+        this.setSize(400, 300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
 }
 
-class MyPanel extends JPanel{
+class MyPanel extends JPanel {
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        g.fillOval(10,10,20,20);
+        g.fillOval(10, 10, 20, 20);
     }
 }
