e464dfc8a0a41348ebd46fca9c9fac87377b82ad
JoshuaZSong <joshuasong94@outlook.com>
2024-12-30 21:36:38 -0700

    event

--- BattleCity/src/com/event/BallMove.java
+++ BattleCity/src/com/event/BallMove.java
@@ -2,9 +2,10 @@ package com.event;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class BallMove extends JFrame {
-    private final MyPanel myPanel;
     MyPanel mp = null;
 
     public static void main(String[] args) {
@@ -12,19 +13,37 @@ public class BallMove extends JFrame {
     }
 
     public BallMove() {
-        myPanel;
+        mp = new MyPanel();
         this.add(mp);
+        this.addKeyListener(mp);
         this.setSize(400, 300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
 }
 
-class MyPanel extends JPanel {
+class MyPanel extends JPanel implements KeyListener {
+
+
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
         g.fillOval(10, 10, 20, 20);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println((char)e.getKeyCode() + " is pressed");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
